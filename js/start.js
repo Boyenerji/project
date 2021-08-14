1ef731ab82d01455c40908f786b7f98d9ee7f4c5
bratik <boyenerji2014@yandex.ru>
2021-08-14 07:54:48 +0300

    first commit

---
+++ js/start.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+
+number = 10;
+console.log(number);
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(2 + 2 * 2 != 8);
+
+
+const isCheched = true,
+      isClose = false;
+
+console.log(isCheched || !isClose);
+

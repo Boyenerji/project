add414276e012d868d5a00ffa6fb027fd4a73c26
bratik <boyenerji2014@yandex.ru>
2021-08-14 20:40:15 +0300

    application 14/08/21

--- js/start.js
+++ js/start.js
@@ -4,8 +4,7 @@ let number = 5;
 const leftBorderWidth = 1;
 
 
-number = 10;
-console.log(number);
+
 
 
 let incr = 10,
@@ -14,15 +13,36 @@ let incr = 10,
 // ++incr;
 // --decr;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(2 + 2 * 2 != 8);
+// console.log(2 + 2 * 2 != 8);
 
 
 const isCheched = true,
       isClose = false;
 
-console.log(isCheched || !isClose);
+// console.log(isCheched || !isClose);
 
-console.log('test');
+
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
